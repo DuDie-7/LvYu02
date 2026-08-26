768cd51b212bf68f92ad9237695bbf1939ffed1b
DuDie <2590224571@qq.com>
2026-08-27 00:41:39 +0800

    修改 API 地址为生产环境

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+// 生产环境直接指向 Railway 后端
+// 如果后期需要区分开发/生产，可以用 import.meta.env 控制，但这里直接用固定地址
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://lvyu01-production.up.railway.app',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
